9fe6c5893fe257851a3a08c680b569f86bb64cd7
Alejandro Yunes <alejandroyunes@outlook.com>
2023-09-08 06:01:39 -0500

    feat: fix

--- src/components/MainThreeColumns/MainColumns.tsx
+++ src/components/MainThreeColumns/MainColumns.tsx
@@ -26,52 +26,25 @@ export default function MainColumns() {
               </div>
             )
           })}
-
         </ CarouselContainer >
       </MobileCardContainer>
 
       <DesktopCardContainer>
-        <div className="three-card__wrapper">
-          <div className="three-card__img">
-            <img src={ResponsiveDesignIcon} />
-          </div>
-          <h2>Responsive Design</h2>
-          <h3>Illustration Gallery</h3>
-          <p>
-            Leading digital agency with solid design and development
-            expertise. We build mobile and web products for startups and
-            design intricate trading platforms.
-          </p>
-          <h6>view more</h6>
-        </div>
-
-        <div className="three-card__wrapper">
-          <div className="three-card__img">
-            <img src={MobileDesignIcon} />
-          </div>
-          <h2>Mobile Design</h2>
-          <h3>Illustration Gallery</h3>
-          <p>
-            Leading digital agency with solid design and development
-            expertise. We build mobile and web products for startups and
-            design intricate trading platforms.
-          </p>
-          <h6>view more</h6>
-        </div>
-
-        <div className="three-card__wrapper">
-          <div className="three-card__img">
-            <img src={RocketAnimation} />
-          </div>
-          <h2>SEO & Marketing</h2>
-          <h3>Illustration Gallery</h3>
-          <p>
-            Leading digital agency with solid design and development
-            expertise. We build mobile and web products for startups and
-            design intricate trading platforms.
-          </p>
-          <h6>view more</h6>
-        </div>
+        {three_columns.cards.map((e, i) => {
+          return (
+            <div className="three-card__wrapper" key={i}>
+              <div className="three-card__img">
+                <img src={e.image} />
+              </div>
+              <h2>{e.title}</h2>
+              <h3>{e.subtitle}</h3>
+              <p>
+                {e.message}
+              </p>
+              {e.viewMore && <h6>view more</h6>}
+            </div>
+          )
+        })}
       </DesktopCardContainer>
     </ThreeCardContainer>
   );

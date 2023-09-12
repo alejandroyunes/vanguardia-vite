05751ef1e89730e2e92d9fab974eb7bba969e246
Alejandro Yunes <alejandroyunes@outlook.com>
2023-09-12 13:54:46 -0500

    fix scroll nav

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,16 @@
 import logo from "../../logo2.svg"
 import { HeaderContainer, Logo, NavContainer, NavItems, HamburgerMenu } from "./header.styled"
 import ColorSwitcher from "../ThemeSwitcher/colorSwitcher"
-import { useLocation, useNavigate, Outlet } from "react-router-dom"
+import { useLocation, useNavigate, Outlet, ScrollRestoration } from "react-router-dom"
 import { useState } from "react"
 
 export default function Header() {
 
-  const location = useLocation()
   let navigate = useNavigate()
-
+  const location = useLocation()
   const { pathname } = location
-  const [ isActive, setIsActive ] = useState(false);
+
+  const [isActive, setIsActive] = useState(false);
 
   const links = [
     { text: 'Inicio', path: '/' },
@@ -29,48 +29,49 @@ export default function Header() {
 
   return (
     <>
-    <HeaderContainer>
-      <Logo className="nav-logo">
-        <img src={logo} alt="Alejandro Web Logo" />
-      </Logo>
-      <NavContainer>
-        <ColorSwitcher />
-        <NavItems>
-          <ul>
-            {links.map((link) => (
-              <li
-                key={link.path}
-                onClick={() => handleClick(link.path)}
-                className={pathname === link.path ? 'active' : ''}
-              >
-                {link.text}
-              </li>
-            ))}
-          </ul>
-        </NavItems>
-        <HamburgerMenu>
-          <div id="toggleMenu">
-            <input
-              type="checkbox"
-              checked={isActive}
-              // defaultChecked={isActive}
-              onChange={() => setIsActive(!isActive)}
-            />
-            <span></span>
-            <span></span>
-            <span></span>
-            <ul id="menu">
+      <HeaderContainer>
+        <Logo className="nav-logo">
+          <img src={logo} alt="Alejandro Web Logo" />
+        </Logo>
+        <NavContainer>
+          <ColorSwitcher />
+          <NavItems>
+            <ul>
               {links.map((link) => (
-                <li key={link.path} onClick={() => handleClick(link.path, true)}>
+                <li
+                  key={link.path}
+                  onClick={() => handleClick(link.path)}
+                  className={pathname === link.path ? 'active' : ''}
+                >
                   {link.text}
                 </li>
               ))}
             </ul>
-          </div>
-        </HamburgerMenu>
-      </NavContainer>
-    </HeaderContainer>
-    <Outlet/>
+            <ScrollRestoration />
+          </NavItems>
+          <HamburgerMenu>
+            <div id="toggleMenu">
+              <input
+                type="checkbox"
+                checked={isActive}
+                // defaultChecked={isActive}
+                onChange={() => setIsActive(!isActive)}
+              />
+              <span></span>
+              <span></span>
+              <span></span>
+              <ul id="menu">
+                {links.map((link) => (
+                  <li key={link.path} onClick={() => handleClick(link.path, true)}>
+                    {link.text}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </HamburgerMenu>
+        </NavContainer>
+      </HeaderContainer >
+      <Outlet />
     </>
   )
 }
---
+++ src/main/layout.tsx
@@ -0,0 +1,20 @@
+import { ThemeProvider } from "styled-components"
+import { darkTheme } from "../styles/themes/darkTheme.tsx"
+import { useLocalStorage } from "usehooks-ts"
+
+import { GlobalStyle } from "../styles/themes/GlobalStyle.tsx"
+import Header from "../components/Header/Header.tsx"
+import Footer from '../components/Footer/Footer.tsx'
+
+
+export default function VanguardiaApp() {
+	const [theme] = useLocalStorage("theme", darkTheme)
+
+	return (
+		<ThemeProvider theme={theme}>
+			<GlobalStyle />
+			<Header />
+			<Footer /> 
+		</ThemeProvider>
+	)
+}

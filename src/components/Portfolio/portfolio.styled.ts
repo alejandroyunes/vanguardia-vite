4616598edb0096352fe582937c0a5eb117814004
alejandro yunes <alejandroyunes@outlook.com>
2023-09-22 01:33:53 -0500

    fix portafolio

--- src/components/Portfolio/portfolio.styled.ts
+++ src/components/Portfolio/portfolio.styled.ts
@@ -1,134 +1,62 @@
 import styled from "styled-components"
 
 export const PortfolioContainer = styled.div`
-  h1 {
-		font-weight: bold;
-		color: ${({ theme }) => theme.palette.primary.main};
-		font-size: 2.8rem;
-		line-height: 1.2;
-	}
-	h2 {
-		color: ${({ theme }) => theme.textColor};
-		font-size: 1.2rem;
-		line-height: 2rem;
-		padding: 16px 0;
-    font-weight: 400;
-	}
-.portfolio-page-hero {
-	text-align: center;
-	margin: 0 auto;
-	width: 100%;
-	img {
-		max-width: 100%;
-	}
-}
-
-.portfolio-billboard {
-	display: grid;
-  max-width: ${({ theme }) => theme.breakpoint.md};
-	min-height: 100%;
-	grid-gap: 30px;
-	margin: 0px auto 80px;
-  padding: 0 24px;
-  grid-template-columns: repeat(2, 1fr);
-	grid-template-rows: repeat(1, auto);
-	grid-template-areas: "portfolio-billboard-child-one portfolio-billboard-child-two";
-
-  .buttons {
-		display: flex;
-		margin-top: 16px;
-	}
-
-	.portfolio-billboard-child-one {
-		grid-area: portfolio-billboard-child-one;
-		margin: auto 0;
-	}
-	.portfolio-billboard-child-two {
-		grid-area: portfolio-billboard-child-two;
-    img {
-			width: 100%;
-			max-height: auto;
-			}
-	}
-}
-
-@media (max-width: ${({ theme }) => theme.breakpoint.xs}) {
-	.portfolio-billboard {
-	  grid-gap: 30px 30px;
-	  grid-template-columns: repeat(1, auto);
-	  grid-template-rows: repeat(2, auto);
-	  grid-template-areas:
-		"portfolio-billboard-child-one"
-		"portfolio-billboard-child-two";
-	  margin: 0px auto 60px;
+	.portfolio-billboard, 
+	.portfolio-billboard-two {
+		display: grid;
+		max-width: ${({ theme }) => theme.breakpoint.md};
+		min-height: 100%;
+		grid-gap: 30px;
+		margin: 0px auto 80px;
+		padding: 0 24px;
+		grid-template-columns: repeat(2, .5fr);
 		h1 {
-			font-size: 2em;
+			font-weight: bold;
+			color: ${({ theme }) => theme.palette.primary.main};
+			font-size: 2.8rem;
+			line-height: 1.2;
 		}
-		.portfolio-billboard-child-two {
-			grid-area: portfolio-billboard-child-two;
-      img {
-			  width: 100%;
-			  max-height: auto;
-			  cursor: pointer;
-		  }
+		h2 {
+			color: ${({ theme }) => theme.textColor};
+			font-size: 1.2rem;
+			line-height: 2rem;
+			padding: 16px 0;
+			font-weight: 400;
+		}
+		.buttons {
+			display: flex;
+			margin-top: 16px;
+		}
+		.child-two {
+			margin: auto 0;
+			img {
+				width: 100%;
+				max-height: auto;
+				}
+		}
+		@media (max-width: ${({ theme }) => theme.breakpoint.sm}) {
+			grid-gap: 30px 30px;
+			grid-template-columns: repeat(1, auto);
+			grid-template-rows: repeat(2, auto);
+			margin: 0px auto 60px;
+			h1 {
+				font-size: 2em;
+			}
 		}
 	}
-}
-
+	
 .portfolio-billboard-two {
-	display: grid;
-	min-height: 100%;
-  max-width: ${({ theme }) => theme.breakpoint.md};
-  margin: 0px auto 80px;
-  grid-gap: 30px;
-  padding: 0 24px;
-	grid-template-columns: repeat(2, 1fr);
-	grid-template-rows: repeat(1, auto);
-	grid-template-areas: "portfolio-billboard-two-child-one portfolio-billboard-two-child-two";
-  
-  .buttons {
-		display: flex;
-		margin-top: 16px;
-	}
-	.portfolio-billboard-two-child-one {
-		grid-area: portfolio-billboard-two-child-one;
+	.child-one {
+		margin: auto 0;
 		img {
 			width: 100%;
 			max-height: auto;
 		}
 	}
-	.portfolio-billboard-two-child-two {
-		grid-area: portfolio-billboard-two-child-two;
+	@media (max-width: ${({ theme }) => theme.breakpoint.sm}){
+	.child-two {
+		order: -1;
 	}
-	
 }
-
-@media (max-width: ${({ theme }) => theme.breakpoint.xs}){
-	.portfolio-billboard-two {
-    grid-gap: 30px 30px;
-	  grid-template-columns: repeat(1, auto);
-	  grid-template-rows: repeat(2, auto);
-	  grid-template-areas:
-		  "portfolio-billboard-two-child-two"
-		  "portfolio-billboard-two-child-one";
-	  margin: 60px auto;
-
-	.portfolio-billboard-two-child-one {
-		grid-area: portfolio-billboard-two-child-one;
-			img {
-			width: 100%;
-			max-height: auto;
-			cursor: pointer;
-		}
-	}
-	.portfolio-billboard-two-child-two {
-		grid-area: portfolio-billboard-two-child-two;
-		padding-right: 24px;
-		margin: auto 0;
-		}
-		h1 {
-			font-size: 2em;
-		}
-	}
 }
 `

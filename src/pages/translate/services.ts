1be6aa810d6676236a2ed07df4335d97281a13f3
alejandro yunes <alejandroyunes@outlook.com>
2023-10-01 09:14:42 -0500

    add services

--- src/pages/translate/services.ts
+++ src/pages/translate/services.ts
@@ -1,5 +1,5 @@
 export const spanish = {
-  aboutUs: {
+  services: {
     title: "Somos una agencia de desarrollo web con más de 4 años de experiencia en el mercado, respaldada por una sólida trayectoria en el campo de la tecnología de la información. Utilizamos tecnologías de vanguardia como JavaScript, TypeScript y React para crear aplicaciones web de alta calidad.",
     subtitle: "Diseño web y móvil altamente personalizado, utilizando tecnologías de última generación como React, Next.js y React Native.",
     alt: "Proyectos de desarrollo web"
@@ -7,7 +7,7 @@ export const spanish = {
 }
 
 export const english = {
-  aboutUs: {
+  services: {
     title: "We are a web development agency with more than 4 years of experience in the market, backed by a solid professional history in the field of information technology. We use cutting-edge technologies like JavaScript, TypeScript, and React to create high-quality web applications.",
     subtitle: "Highly customized web and mobile design, using cutting-edge technologies like React, Next.js, and React Native.",
     alt: "Web development projects"

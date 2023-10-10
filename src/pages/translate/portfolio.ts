44f9feff4f056c7137b373c4020ced58c3e9bcd3
alejandro yunes <alejandroyunes@outlook.com>
2023-10-09 21:19:52 -0500

    adding images to a cdn

--- src/pages/translate/portfolio.ts
+++ src/pages/translate/portfolio.ts
@@ -1,9 +1,3 @@
-import portfolioRestaurant from './images/portfolio/restaurant.webp'
-import iphoneCase from "./images/portfolio/iphone.webp"
-import drPuerta from "./images/portfolio/drpuerta.webp"
-import hotels from "./images/portfolio/hotels.webp"
-import punkies from "./images/portfolio/punkies.webp"
-
 export const spanish = {
   title: "Portafolio",
   subtitle: "Nuestro portafolio encontrarás una amplia variedad de ejemplos que incluyen la creación de sitios web y aplicaciones web, abarcando desde proyectos personales y colaborativos hasta trabajos para empresas privadas.",
@@ -12,7 +6,7 @@ export const spanish = {
     {
       title: 'Punkies y Cerebro',
       subtitle: "Construimos este sitio web utilizando tecnologías de vanguardia como Next, Strapi y Mongo. Nuestro énfasis se centra en un diseño único y altamente flexible que garantiza una completa capacidad de respuesta en todos los dispositivos.",
-      image: punkies,
+      image: 'https://res.cloudinary.com/dcpdkock3/image/upload/v1696903838/portfolio/v1mocn0dzxlin7t2qa1n.webp',
       alt: "Aplicación web para punkies y cerebro",
       url: "https://punkiesycerebro.com",
       technologies: ['Next', 'SCSS', 'Styled', 'Mongo', 'TypeScript'],
@@ -21,16 +15,16 @@ export const spanish = {
     {
       title: 'Dr Puerta',
       subtitle: "Una Landing Page desarrollada en Next, caracterizada por un diseño original y altamente adaptable. Este sitio tiene componentes únicos que garantizan una experiencia original y totalmente responsive en cualquier dispositivo.",
-      image: drPuerta,
+      image: 'https://res.cloudinary.com/dcpdkock3/image/upload/v1696903838/portfolio/t8gzzfw9wo74clefqi2v.webp',
       alt: "Aplicación web para dr puerta",
-      url: "https://drpuerta.com",
+      url: "https://'https://res.cloudinary.com/dcpdkock3/image/upload/v1696903838/portfolio/t8gzzfw9wo74clefqi2v.webp'.com",
       technologies: ['Next', 'SCSS', 'Styled', 'TypeScript'],
-      github: "https://github.com/alejandroyunes/drpuerta"
+      github: "https://github.com/alejandroyunes/'https://res.cloudinary.com/dcpdkock3/image/upload/v1696903838/portfolio/t8gzzfw9wo74clefqi2v.webp'"
     },
     {
       title: 'Experiencia Japonesa',
       subtitle: "Nuestro último estudio de caso presenta una página web desarrollada exclusivamente con HTML, CSS y JavaScript vanilla. Se caracteriza por su diseño original y total adaptabilidad, garantizando una experiencia óptima en todos los dispositivos.",
-      image: portfolioRestaurant,
+      image: 'https://res.cloudinary.com/dcpdkock3/image/upload/v1696903839/portfolio/nkv4zg7ci419ytsmimwg.webp',
       alt: "Aplicación web estudio de caso restaurante Japones",
       url: "https://capable-creponne-253cb7.netlify.app/",
       technologies: ['HTML', 'CSS', 'JavaScript'],
@@ -39,7 +33,7 @@ export const spanish = {
     {
       title: 'Iphone 12',
       subtitle: "Te presentamos un emocionante estudio de caso de una página web creada con HTML, CSS y JavaScript vanilla. Destaca por su diseño auténtico y total adaptabilidad, garantizando una experiencia excepcional en todos los dispositivos.",
-      image: iphoneCase,
+      image: 'https://res.cloudinary.com/dcpdkock3/image/upload/v1696903839/portfolio/ohcmabxhgemc3qlawbel.webp',
       alt: "Aplicación web estudio de caso de iphone 12",
       url: "https://gentle-bonbon-985a86.netlify.app/",
       technologies: ['HTML', 'CSS', 'JavaScript'],
@@ -48,11 +42,11 @@ export const spanish = {
     {
       title: 'Hoteles.com',
       subtitle: "Estudio de caso: Página en HTML, CSS, JavaScript vanilla. Diseño original y responsive. Licencia Premium de imagenes (uso ilimitado sin atribución)",
-      image: hotels,
-      alt: "Case study web app for hotels",
+      image: 'https://res.cloudinary.com/dcpdkock3/image/upload/v1696903838/portfolio/lnzoyc3jdf718jggxx6s.webp',
+      alt: "Case study web app for 'https://res.cloudinary.com/dcpdkock3/image/upload/v1696903838/portfolio/lnzoyc3jdf718jggxx6s.webp'",
       url: "https://hoteles-react.netlify.app/Rooms",
       technologies: ['HTML', 'CSS', 'JavaScript'],
-      github: "https://github.com/alejandroyunes/hotels-app-with-react"
+      github: "https://github.com/alejandroyunes/'https://res.cloudinary.com/dcpdkock3/image/upload/v1696903838/portfolio/lnzoyc3jdf718jggxx6s.webp'-app-with-react"
     }
   ]
 }
@@ -65,7 +59,7 @@ export const english = {
       {
         title: 'Punkies y Cerebro',
         subtitle: "We built this website with cutting-edge technologies such as Next, Strapi, and Mongo. Our emphasis is on unique and highly flexible design, guaranteeing complete responsiveness on all devices.",
-        image: punkies,
+        image: 'https://res.cloudinary.com/dcpdkock3/image/upload/v1696903838/portfolio/v1mocn0dzxlin7t2qa1n.webp',
         alt: "Web application for punkies y cerebro",
         url: "https://punkiesycerebro.com",
         technologies: ['Next', 'SCSS', 'Styled', 'Mongo', 'TypeScript'],
@@ -74,16 +68,16 @@ export const english = {
       {
         title: 'Dr Puerta',
         subtitle: "A Landing Page developed in Next, characterized by an original and highly adaptable design. This site features unique components that ensure an original and fully responsive experience on any device.",
-        image: drPuerta,
+        image: 'https://res.cloudinary.com/dcpdkock3/image/upload/v1696903838/portfolio/t8gzzfw9wo74clefqi2v.webp',
         alt: "Web application for dr puerta",
-        url: "https://drpuerta.com",
+        url: "https://'https://res.cloudinary.com/dcpdkock3/image/upload/v1696903838/portfolio/t8gzzfw9wo74clefqi2v.webp'.com",
         technologies: ['Next', 'SCSS', 'Styled', 'TypeScript'],
-        github: "https://github.com/alejandroyunes/drpuerta"
+        github: "https://github.com/alejandroyunes/'https://res.cloudinary.com/dcpdkock3/image/upload/v1696903838/portfolio/t8gzzfw9wo74clefqi2v.webp'"
       },
       {
         title: 'Japanese Experience',
         subtitle: "Our latest case study showcases a website developed exclusively using standard HTML, CSS, and JavaScript. It is distinguished by its original design and complete adaptability, ensuring an optimal experience on all devices.",
-        image: portfolioRestaurant,
+        image: 'https://res.cloudinary.com/dcpdkock3/image/upload/v1696903839/portfolio/nkv4zg7ci419ytsmimwg.webp',
         alt: "Web application for Japanese restaurant",
         url: "https://capable-creponne-253cb7.netlify.app/",
         technologies: ['HTML', 'CSS', 'JavaScript'],
@@ -92,7 +86,7 @@ export const english = {
       {
         title: 'Iphone 12',
         subtitle: "We present you an exciting case study of a website created with HTML, CSS, and vanilla JavaScript. It stands out for its authentic design and complete adaptability, ensuring an exceptional experience on all devices.",
-        image: iphoneCase,
+        image: 'https://res.cloudinary.com/dcpdkock3/image/upload/v1696903839/portfolio/ohcmabxhgemc3qlawbel.webp',
         alt: "Case study for iphone web app",
         url: "https://gentle-bonbon-985a86.netlify.app/",
         technologies: ['HTML', 'CSS', 'JavaScript'],
@@ -101,11 +95,11 @@ export const english = {
       {
         title: 'Hoteles.com',
         subtitle: "Case study: Website in HTML, CSS, and vanilla JavaScript. Original and Responsive Design. Premium Image License (Unlimited Usage without Attribution).",
-        image: hotels,
-        alt: "Case study app about hotels",
+        image: 'https://res.cloudinary.com/dcpdkock3/image/upload/v1696903838/portfolio/lnzoyc3jdf718jggxx6s.webp',
+        alt: "Case study app about 'https://res.cloudinary.com/dcpdkock3/image/upload/v1696903838/portfolio/lnzoyc3jdf718jggxx6s.webp'",
         url: "https://hoteles-react.netlify.app/Rooms",
         technologies: ['HTML', 'CSS', 'JavaScript'],
-        github: "https://github.com/alejandroyunes/hotels-app-with-react"
+        github: "https://github.com/alejandroyunes/'https://res.cloudinary.com/dcpdkock3/image/upload/v1696903838/portfolio/lnzoyc3jdf718jggxx6s.webp'-app-with-react"
       }
     ]
 }

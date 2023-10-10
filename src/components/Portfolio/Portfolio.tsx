576767f05be1c0d88baf47acb726be59e48fa33a
alejandro yunes <alejandroyunes@outlook.com>
2023-10-10 08:52:52 -0500

    remove portfolio title and subtitle

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -1,5 +1,4 @@
 import { PortfolioContainer } from "./portfolio.styled"
-import Title from "../../components/Title/Title"
 import Button from "../../components/Button/Button"
 import Technologies from "./Technologies/Technologies"
 import Github from '../../pages/translate/svgs/portfolio/github'
@@ -22,66 +21,64 @@ interface PorfolioProps {
   }[]
 }
 
-export default function Portfolio({ title, items, subtitle, meta }: PorfolioProps) {
+export default function Portfolio({ items, meta }: PorfolioProps) {
 
   const lang = useReadLocalStorage('language')
   return (
 
-      <PortfolioContainer>
+    <PortfolioContainer>
 
       <HelmetProvider>
         <Helmet>
-        <meta name="description" content={meta}/>
+          <meta name="description" content={meta} />
         </Helmet>
       </HelmetProvider>
 
-        {/* <Title title={title} message={subtitle} /> */}
+      {items.map((e, i) => (
+        <div className="portfolio-billboard" key={i}>
+          <div className="child-one">
 
-        {items.map((e, i) => (
-          <div className="portfolio-billboard" key={i}>
-            <div className="child-one">
+            <div className="item-info">
+              <h2>
+                <strong>
+                  {e.title}
+                </strong>
+              </h2>
+              <p>
+                {e.subtitle}
+              </p>
+            </div>
 
-              <div className="item-info">
-                <h2>
-                  <strong>
-                    {e.title}
-                  </strong>
-                </h2>
-                <p>
-                  {e.subtitle}
-                </p>
-              </div>
+            <Technologies technologies={e.technologies} />
 
-              <Technologies technologies={e.technologies} />
-
-              <div className="buttons">
+            <div className="buttons">
+              <Button
+                label={lang === 'english' ? 'view site' : 'ver sitio'}
+                category="primary"
+                size="sm"
+                icon={Website}
+                onClick={() =>
+                  window.open(e.url, "_blank")
+                }
+              />
+              <div className="portfolio-repo">
                 <Button
-                  label={lang === 'english' ? 'view site' : 'ver sitio'}
-                  category="primary"
-                  size="sm"
-                  icon={Website}
+                  label={lang === 'english' ? 'view repo' : 'ver repo'}
+                  icon={Github}
                   onClick={() =>
-                    window.open(e.url, "_blank")
+                    window.open(e.github, "_blank")
                   }
                 />
-                <div className="portfolio-repo">
-                  <Button
-                    label={lang === 'english' ? 'view repo' : 'ver repo'}
-                    icon={Github}
-                    onClick={() =>
-                      window.open(e.github, "_blank")
-                    }
-                  />
-                </div>
               </div>
             </div>
-
-            <div className="child-two">
-              <img src={e.image} alt={e.alt} width="600" height="400" />
-            </div>
           </div>
-        ))
-        }
-      </PortfolioContainer >
+
+          <div className="child-two">
+            <img src={e.image} alt={e.alt} width="600" height="400" />
+          </div>
+        </div>
+      ))
+      }
+    </PortfolioContainer >
   )
 }
